6e967d58c25b97f8e2c8e780823b9180857c5e3d
Megan Lee <megselayne@gmail.com>
2020-09-16 18:14:02 -0400

    testing promise all with api

--- services/bookHelpers.js
+++ services/bookHelpers.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const fetch = require('node-fetch');
 let key = process.env.GOOGLE_KEY;
 
-const getBooks = () => {
+//temporary array to represent saved books
+const savedSelections = ['p7uGzQEACAAJ', 'FylCUanlGhAC', 'ltClDwAAQBAJ', 'r6c8DAAAQBAJ', 'MWB3AgAAQBAJ', '5GbdTc9OJ78C']
+const searchBooks = () => {
   fetch(`https://www.googleapis.com/books/v1/volumes?q=pride+and+prejudice&key=${key}`)  
   .then(res => res.json())
   .then((res) => {
@@ -10,4 +12,22 @@ const getBooks = () => {
   })
 }
 
-getBooks()
+const getSavedBooks = () => {
+  const fetches = savedSelections.map((book) => {
+    return fetch(`https://www.googleapis.com/books/v1/volumes/${book}`)
+  })
+  return Promise.all(fetches)
+  .then((res) => {
+    return Promise.all(res.map((el) => {
+      return el.json()
+    }))
+  })
+  .then((data) => {
+    console.log(data)
+  })
+}
+
+module.exports = {
+  searchBooks,
+  getSavedBooks
+}
