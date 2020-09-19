2740d7cc5f75b4b42df69ea0b18ad94ce2efa46a
Megan Lee <megselayne@gmail.com>
2020-09-19 16:13:48 -0400

    pushing up for help troubleshooting

--- services/bookHelpers.js
+++ services/bookHelpers.js
@@ -3,6 +3,7 @@ const Shelves = require('../models/Shelves');
 const fetch = require('node-fetch');
 let key = process.env.GOOGLE_KEY;
 
+//reduce shelf objects to one object per shelf, with array of google book ids as value for google_book_ids: [id, id, id]
 const shelfReducer = (arr) => {
   return arr.reduce((accum, current) => {
     const found = accum.find(el => el.id === current.id);
@@ -31,14 +32,40 @@ const searchBooks = () => {
       console.log(res)
   })
 }
-//reduce shelf objects to one object, with array of google book ids as value for books: [id, id, id]
 
 const getPublicBooks = (req, res, next) => {
   Shelves.getPublicShelfBooks()
   .then(shelves => {
+    const reduced = shelfReducer(shelves);
+    return reduced.map(shelf => {
+      const fetches = shelf.google_book_ids.map(book => {
+        return fetch(`https://www.googleapis.com/books/v1/volumes/${book}`)
+      })
+      Promise.all(fetches.map(el => {
+        return el.json()
+      }))
+      .then(data => {
+        console.log(data)
+      })
 
+    })
+  })
+  .catch((err) => {
+    console.log(err);
   })
 }
+
+const getSingleBook = (book) => {
+  fetch(`https://www.googleapis.com/books/v1/volumes/${book}`)
+  .then(data => data.json())
+  .then(data => {
+    return data
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
 
 const getSavedBooks = () => {
   const fetches = savedSelections.map((book) => {
@@ -55,7 +82,10 @@ const getSavedBooks = () => {
   })
 }
 
+
 module.exports = {
   searchBooks,
-  getSavedBooks
+  getSavedBooks,
+  getSingleBook,
+  getPublicBooks
 }
