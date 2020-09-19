6d4ce376f517ee1c8a45634e91ddd68922b13492
Megan Lee <megselayne@gmail.com>
2020-09-19 13:58:13 -0400

    successful array object reducer

--- services/bookHelpers.js
+++ services/bookHelpers.js
@@ -1,6 +1,26 @@
 require('dotenv').config();
+const Shelves = require('../models/Shelves');
 const fetch = require('node-fetch');
 let key = process.env.GOOGLE_KEY;
+
+const shelfReducer = (arr) => {
+  return arr.reduce((accum, current) => {
+    const found = accum.find(el => el.id === current.id);
+    if(!found){
+      accum.push(
+        {
+          shelf_name: current.shelf_name,
+          id: current.id,
+          google_book_ids: [current.google_book_id]
+        }
+      )
+    }
+    else {
+      found.google_book_ids.push(current.google_book_id)
+    }
+    return accum
+  }, [])
+}
 
 //temporary array to represent saved books
 const savedSelections = ['p7uGzQEACAAJ', 'FylCUanlGhAC', 'ltClDwAAQBAJ', 'r6c8DAAAQBAJ', 'MWB3AgAAQBAJ', '5GbdTc9OJ78C']
@@ -9,6 +29,14 @@ const searchBooks = () => {
   .then(res => res.json())
   .then((res) => {
       console.log(res)
+  })
+}
+//reduce shelf objects to one object, with array of google book ids as value for books: [id, id, id]
+
+const getPublicBooks = (req, res, next) => {
+  Shelves.getPublicShelfBooks()
+  .then(shelves => {
+
   })
 }
 
