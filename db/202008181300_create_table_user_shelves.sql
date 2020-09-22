81b57de1944d88e04b16785677c75a182d63b8a1
Megan Lee <megselayne@gmail.com>
2020-09-22 12:16:09 -0400

    schema udpates

--- db/202008181300_create_table_user_shelves.sql
+++ db/202008181300_create_table_user_shelves.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS user_shelves(
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
-    shelf_id INTEGER REFERENCES shelves(id)
+    shelf_id INTEGER REFERENCES shelves(id) UNIQUE
 );
--- db/202008201033_alter_table_user_books.sql
+++ db/202008201033_alter_table_user_books.sql
@@ -1,5 +1,2 @@
-ALTER TABLE user_shelves
-ALTER COLUMN shelf_id INTEGER REFERENCES shelves(id) UNIQUE;
-
 ALTER TABLE user_books
 ADD COLUMN IF NOT EXISTS shelf_id INTEGER REFERENCES user_shelves(shelf_id);
