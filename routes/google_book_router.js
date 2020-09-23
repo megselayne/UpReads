fe8163d759057d5de8fa65a4cc3df75d7da64e2e
Megan Lee <megselayne@gmail.com>
2020-09-23 09:56:23 -0400

    adjusted route name

--- routes/google_book_router.js
+++ routes/google_book_router.js
@@ -5,7 +5,7 @@ const bookHelpers = require('../services/bookHelpers');
 gBookRouter.get('/home', bookHelpers.getPublicBooks, (req, res) => {
     res.json(res.locals.publicBooks)
 })
-gBookRouter.get('single/:id', bookHelpers.getSingleBook, (req, res) => {
+gBookRouter.get('/single/:id', bookHelpers.getSingleBook, (req, res) => {
     res.json(res.locals.singleBook);
 })
 gBookRouter.post('/search', bookHelpers.searchBooks, (req, res) => {
