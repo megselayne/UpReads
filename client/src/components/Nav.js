611236b1595e1f2360e3bcacdb3dc5952b309d45
Megan Lee <megselayne@gmail.com>
2020-11-07 15:20:56 -0800

    nav tweaks

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -7,15 +7,17 @@ class Navigation extends Component {
         return (
             <Navbar className='nav-outer' expand="lg">
                 <Navbar.Brand>
+                    <Nav.Link className='navlinks' href='/'>
                     <img className='logo' src='https://www.flaticon.com/premium-icon/icons/svg/2847/2847689.svg' />
+                    </Nav.Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className='nav-container'>
-                <Nav.Link href='/'>Home</Nav.Link>
-                <Nav.Link href='/search'>Search</Nav.Link>
-                    {this.props.auth && <Nav.Link href='/user/profile'>Profile</Nav.Link>}
-                    {this.props.auth ? (<Nav.Link className='log' href='#auth/logout' onClick={this.props.logout}>Logout</Nav.Link> ): (<Nav.Link className='log' href='/auth/login'>Login</Nav.Link>)}
+                <Nav.Link className='navlinks' href='/'>Home</Nav.Link>
+                <Nav.Link className='navlinks' href='/search'>Search</Nav.Link>
+                    {this.props.auth && <Nav.Link className='navlinks' href='/user/profile'>Profile</Nav.Link>}
+                    {this.props.auth ? (<Nav.Link className='navlinks' href='#auth/logout' onClick={this.props.logout}>Logout</Nav.Link> ): (<Nav.Link className='navlinks' href='/auth/login'>Login</Nav.Link>)}
                 </Nav>
                 </Navbar.Collapse>
             </Navbar>
